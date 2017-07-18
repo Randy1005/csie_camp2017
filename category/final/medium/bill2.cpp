8349c7cf5acfed782d0858f5e751882c1a296a86
Randy1005 <randychang.10@gmail.com>
2017-07-18 12:13:50 +0800

    answer updated

--- category/final/medium/bill2.cpp
+++ category/final/medium/bill2.cpp
@@ -4,30 +4,19 @@ int main(){
 	float studyTime[5] = {4,5,2,3,6};
 	float degrees[5] = {3,3,2,2,3};
 	float cost[5];
-	bool saveOrNot[5];
 	float totalCost = 0;
-	float oriCost = 0;
-	for(int i=0;i<5;i++){
-		saveOrNot[i] = true;
-	}
 
 	for(int i=0;i<5;i++){
 		cost[i] = degrees[i] / studyTime[i];
-		if(cost[i] <= 0.6){
-			saveOrNot[i] = false;
+		if(cost[i] > 0.6001){
+			totalCost += cost[i];
 		}
 		//cout << cost[i] << endl;
 	}
-	//original cose
-	for(int i=0;i<5;i++){
-		oriCost += cost[i];
-	}
-	for(int i=0;i<5;i++){
-		if(saveOrNot[i])
-			totalCost += cost[i];
-	}
+	for(int i=0;i<5;i++)
+		cout << cost[i] << " ";
+	cout << endl;
 	cout << "total cost: " << totalCost << endl;
-	cout << "original cost: " << oriCost << endl;
 	
 	
 }
